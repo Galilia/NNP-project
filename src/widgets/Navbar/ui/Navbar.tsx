53b213203c3ec76d334cf65e6689871255419f9d
Galilia <iliagalperin@gmail.com>
2023-02-18 15:10:59 +0200

    upgrade sidebar, add button themes

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -12,14 +12,7 @@ export const Navbar = ({ className }: NavbarProps) => {
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-                    {t('main_page')}
-                </AppLink>
-                <AppLink theme={AppLinkTheme.RED} to="/about">
-                    {t('about_page')}
-                </AppLink>
-            </div>
+            <div className={cls.links} />
         </div>
     );
 };
