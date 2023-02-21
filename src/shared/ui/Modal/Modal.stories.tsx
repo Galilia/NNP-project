85d20b6babc9b3bbc9112472368e63cc93ed289d
Galilia <iliagalperin@gmail.com>
2023-02-21 10:25:17 +0200

    add modal, portal, change header

---
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Modal } from './Modal';
+
+export default {
+    title: 'shared/Modal',
+    component: Modal,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    isOpen: true,
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab architecto asperiores commodi consequatur dolorem facilis incidunt itaque libero mollitia optio quasi, qui repellendus tenetur voluptates. Commodi fugiat hic officiis?\n',
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+    isOpen: true,
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab architecto asperiores commodi consequatur dolorem facilis incidunt itaque libero mollitia optio quasi, qui repellendus tenetur voluptates. Commodi fugiat hic officiis?\n',
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
