b85b7fd66980692ee651737046b5154003f2507e
Galilia <iliagalperin@gmail.com>
2023-03-03 18:37:58 +0200

    added apiUrl-global, and extra and put no-unused-vars to off

--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -22,5 +22,5 @@ export function buildCssLoader(isDev: boolean) {
             // Compiles Sass to CSS
             'sass-loader',
         ],
-    }
+    };
 }
--- src/index.tsx
+++ src/index.tsx
@@ -8,14 +8,14 @@ import App from './app/App';
 import '../firebase';
 
 render(
-    <StoreProvider>
-        <BrowserRouter>
+    <BrowserRouter>
+        <StoreProvider>
             <ErrorBoundary>
                 <ThemeProvider>
                     <App />
                 </ThemeProvider>
             </ErrorBoundary>
-        </BrowserRouter>
-    </StoreProvider>,
+        </StoreProvider>
+    </BrowserRouter>,
     document.getElementById('root'),
 );
