fc684c8a99ecef8e32836de05b7f974c0732ee60
Galilia <iliagalperin@gmail.com>
2023-03-31 09:21:19 +0300

    add description to main page and fix translation on articles page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,33 @@
 import { useTranslation } from 'react-i18next';
 import { Page } from 'widgets/Page/Page';
+import cls from './MainPage.module.scss';
 
 const MainPage = () => {
     const { t } = useTranslation();
 
     return (
         <Page>
-            {t('main_page')}
+            {/* eslint-disable i18next/no-literal-string */}
+            <h1>List of developed entities and features</h1>
+            <h2>Entities:</h2>
+            <ul>
+                <li>Autocomplete</li>
+                <li>Comment</li>
+                <li>Counter</li>
+                <li>Currency</li>
+                <li>Country</li>
+                <li>Profile</li>
+                <li>User</li>
+                <li>TagsInput</li>
+            </ul>
+
+            <h2 className={cls.header}>Features:</h2>
+            <ul>
+                <li>addCommentForm</li>
+                <li>AuthByUsername</li>
+                <li>ScrollSave</li>
+                <li>LangSwitcher</li>
+            </ul>
         </Page>
     );
 };
