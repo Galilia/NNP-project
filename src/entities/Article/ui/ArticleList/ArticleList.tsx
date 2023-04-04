f24d716495997bdd88cf71210ddf898aa8c25fe9
Galilia <iliagalperin@gmail.com>
2023-04-04 15:50:39 +0300

    add virtuoso library and change ArticleList

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -5,6 +5,8 @@ import {
 } from 'entities/Article/ui/ArticleListItem/ArticleListItemSkeleton';
 import { Text, TextSize } from 'shared/ui/Text/Text';
 import { useTranslation } from 'react-i18next';
+import { Virtuoso, VirtuosoGrid } from 'react-virtuoso';
+import { ArticlesPageFilters } from 'pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters';
 import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
 import { Article, ArticleView } from '../../model/types/article';
 import cls from './ArticleList.module.scss';
@@ -15,6 +17,7 @@ interface ArticleListProps {
     isLoading?: boolean;
     view?: ArticleView;
     target?: HTMLAttributeAnchorTarget;
+    onLoadNextPart?: () => void;
 }
 
 const getSkeletons = (view: ArticleView) => new Array(view === 'GRID' ? 9 : 3)
@@ -30,10 +33,11 @@ export const ArticleList = memo((props: ArticleListProps) => {
         isLoading,
         target,
         view = 'GRID',
+        onLoadNextPart,
     } = props;
     const { t } = useTranslation();
 
-    const renderArticle = (article: Article) => (
+    const renderArticle = (index: number, article: Article) => (
         <ArticleListItem
             article={article}
             view={view}
@@ -42,6 +46,15 @@ export const ArticleList = memo((props: ArticleListProps) => {
             target={target}
         />
     );
+
+    const footer = () => {
+        if (isLoading) {
+            return <div>{getSkeletons(view)}</div>;
+        }
+        return null;
+    };
+
+    const header = () => <ArticlesPageFilters className={cls.ArticlesPageFilters} />;
 
     if (!isLoading && !articles.length) {
         return (
@@ -53,10 +66,29 @@ export const ArticleList = memo((props: ArticleListProps) => {
 
     return (
         <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
-            {articles.length > 0
-                ? articles.map(renderArticle)
-                : null}
-            {isLoading && getSkeletons(view)}
+            { view === 'LIST' ? (
+                <Virtuoso
+                    style={{ height: '100%' }}
+                    data={articles}
+                    itemContent={renderArticle}
+                    endReached={onLoadNextPart}
+                    components={{
+                        Header: header,
+                        Footer: footer,
+                    }}
+                />
+            ) : (
+                <VirtuosoGrid
+                    components={{
+                        Header: header,
+                        Footer: footer,
+                    }}
+                    endReached={onLoadNextPart}
+                    data={articles}
+                    itemContent={renderArticle}
+                    listClassName={cls.grid}
+                />
+            )}
         </div>
     );
 });
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,4 +1,3 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { memo, useCallback } from 'react';
 import { ArticleList } from 'entities/Article';
 import {
@@ -8,8 +7,6 @@ import {
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useSelector } from 'react-redux';
-import { Page } from 'widgets/Page/Page';
-import { ArticlesPageFilters } from 'pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters';
 import { useSearchParams } from 'react-router-dom';
 import {
     fetchNextArticlesPage,
@@ -49,18 +46,13 @@ const ArticlesPage = ({ className }: ArticlesPageProps) => {
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount={false}>
-            <Page
-                onScrollEnd={onLoadNextPart}
-                className={classNames(cls.ArticlesPage, {}, [className])}
-            >
-                <ArticlesPageFilters />
-                <ArticleList
-                    isLoading={isLoading}
-                    view={view}
-                    articles={articles}
-                    className={cls.list}
-                />
-            </Page>
+            <ArticleList
+                isLoading={isLoading}
+                view={view}
+                articles={articles}
+                className={cls.list}
+                onLoadNextPart={onLoadNextPart}
+            />
         </DynamicModuleLoader>
     );
 };
