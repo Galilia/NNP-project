572f04c84963471c5de043e4354de49ef0ed5dfb
Galilia <iliagalperin@gmail.com>
2023-04-13 16:46:48 +0300

    add TS isolatedModules and refactor part 2 userConst

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -2,4 +2,4 @@ export { getUserAuthData, getUserInited } from './model/selectors/userSelectors'
 export { getUserRoles, isUserAdmin, isUserManager } from './model/selectors/roleSelectors';
 export { userReducer, userActions } from './model/slice/userSlice';
 export type { UserSchema, User } from './model/types/user';
-export { UserRole } from './model/types/user';
+export { UserRole } from './model/consts/userConsts';
