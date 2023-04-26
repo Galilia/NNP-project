844eeabbb84eb7bbc0cc4398c3387481932ffb1b
Galilia <iliagalperin@gmail.com>
2023-04-26 15:48:10 +0300

    fix sidebar test problem because of useDevice hook for mobile

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -2,6 +2,21 @@ import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from './Sidebar';
 import { componentRender } from '@/shared/lib/tests/componentRender/componentRender';
 
+beforeEach(() => {
+    window.matchMedia = jest.fn().mockImplementation((query) => {
+        return {
+            matches: query === '(max-width: 768px)', // always return true or false based on the query
+            addListener: jest.fn(),
+            removeListener: jest.fn(),
+        };
+    });
+});
+
+afterEach(() => {
+    // @ts-ignore
+    delete window.matchMedia;
+});
+
 describe('Sidebar', () => {
     test('with only first param', () => {
         componentRender(<Sidebar />);
