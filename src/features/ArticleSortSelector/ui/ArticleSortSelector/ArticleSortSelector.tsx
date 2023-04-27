261ef64e30abc5599e67f9f8b4f764920fccd3d8
Galilia <iliagalperin@gmail.com>
2023-04-27 11:58:27 +0300

    relocate ArticleSortSelector.tsx, ArticleViewSelector.tsx, ArticleTypeTabs.tsx to features

---
+++ src/features/ArticleSortSelector/ui/ArticleSortSelector/ArticleSortSelector.tsx
@@ -0,0 +1,68 @@
+import { memo, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { ArticleSortField } from '@/entities/Article';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { SortOrder } from '@/shared/types';
+import { Select, SelectOption } from '@/shared/ui/Select';
+
+import cls from './ArticleSortSelector.module.scss';
+
+interface ArticleSortSelectorProps {
+    className?: string;
+    sort: ArticleSortField;
+    order: SortOrder;
+    onChangeOrder: (newOrder: SortOrder) => void;
+    onChangeSort: (newSort: ArticleSortField) => void;
+}
+
+export const ArticleSortSelector = memo((props: ArticleSortSelectorProps) => {
+    const {
+        className, sort, order, onChangeOrder, onChangeSort,
+    } = props;
+    const { t } = useTranslation();
+
+    const orderOptions = useMemo<SelectOption<SortOrder>[]>(() => [
+        {
+            value: 'asc',
+            content: t('ascending'),
+        },
+        {
+            value: 'desc',
+            content: t('descending'),
+        },
+    ], [t]);
+
+    const sortedFieldOptions = useMemo<SelectOption<ArticleSortField>[]>(() => [
+        {
+            value: ArticleSortField.CREATED,
+            content: t('creation date'),
+        },
+        {
+            value: ArticleSortField.VIEWS,
+            content: t('views'),
+        },
+        {
+            value: ArticleSortField.TITLE,
+            content: t('names'),
+        },
+    ], [t]);
+
+    return (
+        <div className={classNames(cls.ArticleSortSelector, {}, [className])}>
+            <Select
+                options={sortedFieldOptions}
+                label={t('Sort By')}
+                value={sort}
+                onChange={onChangeSort}
+            />
+            <Select
+                options={orderOptions}
+                label={t('By')}
+                value={order}
+                onChange={onChangeOrder}
+                className={cls.order}
+            />
+        </div>
+    );
+});
---
+++ src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,61 @@
+import { memo, SVGProps, VFC } from 'react';
+
+import { ArticleView } from '@/entities/Article';
+import ListIcon from '@/shared/assets/icons/list-24-24.svg';
+import TiledIcon from '@/shared/assets/icons/tiled-24-24.svg';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { Button, ButtonTheme } from '@/shared/ui/Button';
+import { Icon } from '@/shared/ui/Icon';
+
+import cls from './ArticleViewSelector.module.scss';
+
+interface ArticleViewSelectorProps {
+    className?: string;
+    view: ArticleView;
+    onViewClick?: (view: ArticleView) => void;
+}
+
+interface ViewType {
+    view: ArticleView;
+    icon: VFC<SVGProps<SVGSVGElement>>;
+}
+
+const viewTypes: ViewType[] = [
+    {
+        view: 'GRID',
+        icon: TiledIcon,
+    },
+    {
+        view: 'LIST',
+        icon: ListIcon,
+    },
+];
+
+export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
+    const { className, view, onViewClick } = props;
+
+    const onClick = (newView: ArticleView) => () => {
+        if (onViewClick) {
+            onViewClick(newView);
+        }
+    };
+
+    return (
+        <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
+            {
+                viewTypes.map((viewType) => (
+                    <Button
+                        key={viewType.view}
+                        theme={ButtonTheme.CLEAR}
+                        onClick={onClick(viewType.view)}
+                    >
+                        <Icon
+                            Svg={viewType.icon}
+                            className={classNames('', { [cls.notSelected]: viewType.view !== view }, [])}
+                        />
+                    </Button>
+                ))
+            }
+        </div>
+    );
+});
