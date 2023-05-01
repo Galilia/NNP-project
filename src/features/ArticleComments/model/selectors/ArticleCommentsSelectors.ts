450d063ecea091c03d169d5a9837390f8f9ee024
Galilia <iliagalperin@gmail.com>
2023-05-01 10:10:46 +0300

    add appImage in shared, fallback, errorFallback

--- src/features/ArticleComments/model/selectors/ArticleCommentsSelectors.ts
+++ src/features/ArticleComments/model/selectors/ArticleCommentsSelectors.ts
@@ -2,6 +2,6 @@ import { StateSchema } from '@/app/providers/StoreProvider';
 
 export const getAddCommentFormText = (state: StateSchema) => state.addCommentForm?.text ?? '';
 export const getAddCommentFormError = (state: StateSchema) => state.addCommentForm?.error;
-// TODO articleDetailsPage maybe put into addCommentForm
+
 export const getArticleCommentsIsLoading = (state: StateSchema) => state.articleDetailsPage?.comments?.isLoading;
 export const getArticleCommentsError = (state: StateSchema) => state.articleDetailsPage?.comments?.error;
--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -1,6 +1,11 @@
 import { CSSProperties, useMemo } from 'react';
 
+import UserIcon from '@/shared/assets/icons/user-filled.svg';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
+
+import { AppImage } from '../AppImage';
+import { Icon } from '../Icon';
+import { Skeleton } from '../Skeleton';
 
 import cls from './Avatar.module.scss';
 
@@ -9,20 +14,26 @@ interface AvatarProps {
     src?: string;
     alt?: string;
     size?: number;
+    fallbackInverted?: boolean;
 }
 
 export const Avatar = ({
-    className, src, alt, size,
+    className, src, alt, size = 100, fallbackInverted,
 }: AvatarProps) => {
     const mods: Mods = {};
 
     const styles = useMemo<CSSProperties>(() => ({
-        width: size || 100,
-        height: size || 100,
+        width: size,
+        height: size,
     }), [size]);
 
+    const fallback = <Skeleton width={size} height={size} border="50%" />;
+    const errorFallback = <Icon Svg={UserIcon} width={size} height={size} inverted={fallbackInverted} />;
+
     return (
-        <img
+        <AppImage
+            fallback={fallback}
+            errorFallback={errorFallback}
             src={src}
             alt={alt}
             style={styles}
