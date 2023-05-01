1733a23a539195bf3a06b04e1859b730cf6ff47b
Galilia <iliagalperin@gmail.com>
2023-05-01 13:49:29 +0300

    fix Counter.test.tsx

--- src/entities/Counter/ui/Counter.test.tsx
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -1,5 +1,4 @@
-import { userEvent } from '@storybook/testing-library';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 
 import { componentRender } from '@/shared/lib/tests/componentRender/componentRender';
 
@@ -17,7 +16,7 @@ describe('Counter', () => {
         componentRender(<Counter />, {
             initialState: { counter: { value: 10 } },
         });
-        userEvent.click(screen.getByTestId('increment-btn'));
+        fireEvent.click(screen.getByTestId('increment-btn'));
         expect(screen.getByTestId('value-title')).toHaveTextContent('11');
     });
 
@@ -25,7 +24,7 @@ describe('Counter', () => {
         componentRender(<Counter />, {
             initialState: { counter: { value: 10 } },
         });
-        userEvent.click(screen.getByTestId('decrement-btn'));
+        fireEvent.click(screen.getByTestId('decrement-btn'));
         expect(screen.getByTestId('value-title')).toHaveTextContent('9');
     });
 });
