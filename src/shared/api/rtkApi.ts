24dfe30609d5f0f7a613387d90fda57aac353598
Galilia <iliagalperin@gmail.com>
2023-05-03 10:00:36 +0300

    fix bug and add isomorphic-fetch

--- src/shared/api/rtkApi.ts
+++ src/shared/api/rtkApi.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import fetch from 'isomorphic-fetch';
 
 import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
 
@@ -7,6 +8,7 @@ export const rtkApi = createApi({
     baseQuery: fetchBaseQuery(
         {
             baseUrl: __API__,
+            fetchFn: fetch,
             prepareHeaders: (headers) => {
                 const token = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
                 if (token) {
--- src/shared/lib/hooks/useDevice/useDevice.ts
+++ src/shared/lib/hooks/useDevice/useDevice.ts
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 
+const MOBILE_BREAKPOINT = 768;
+
 export const useDevice = () => {
     const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
-        const handleResize = () => setIsMobile(window.matchMedia('(pointer:coarse)').matches);
+        // const handleResize = () => setIsMobile(window.matchMedia('(pointer:coarse)').matches);
+        const handleResize = () => setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
 
         handleResize();
         window.addEventListener('resize', handleResize);
